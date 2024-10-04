962f1dce3594040a64be6e1b258deebc07e8b329
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-04 18:38:53 +0600

    name

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-    // Calculate Money
-
 function calculateMoney(ticketSale) {
     if (ticketSale < 0 || typeof ticketSale !== 'number') {
-        return `Ticket quantity must be a positive number`;
+        return "Invalid Number"; 
     }
+    
     const ticketPrice = 120;
     const guardSalary = 500;
     const lunchFee = 50;
@@ -16,8 +15,6 @@ function calculateMoney(ticketSale) {
     const result = totalRevenue - totalSalary;
     return result;
 }
-
-
 document.getElementById('calculateBtn').addEventListener('click', function() {
     const ticketSale = parseInt(document.getElementById('ticketSale').value);
     const resultElement = document.getElementById('result');
@@ -28,4 +25,26 @@ document.getElementById('calculateBtn').addEventListener('click', function() {
 
 
 
-    // 2. 
+//checkName
+function checkName() {
+    const name = document.getElementById('nameInput').value;
+    const nameResultElement = document.getElementById('nameResult');
+
+    if (typeof name !== 'string' || !/^[a-zA-Z]+$/.test(name)) {
+        nameResultElement.textContent = "invalid"; 
+        return; 
+    }
+
+    const lowerName = name.toLowerCase();
+    const badEndings = ['a', 'y', 'i', 'e', 'o', 'u', 'w'];
+    const lastChar = lowerName.charAt(lowerName.length - 1);
+
+    if(badEndings.includes(lastChar)) {
+        nameResultElement.textContent = "Bad Name"; 
+    }
+    
+    else {
+    nameResultElement.textContent = "Good Name"; 
+    }
+}
+            document.getElementById('checkNameBtn').addEventListener('click', checkName);
